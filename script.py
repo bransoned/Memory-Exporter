8ec898af2a6c952628976bcbeeb301989f43dadb
bransoned <bransonmb@proton.me>
2025-12-10 18:52:12 +0000

    monolith commit. core functionality of downloading jpgs, mp4s, and zips complete

--- script.py
+++ script.py
@@ -1,5 +1,32 @@
-#from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 import re
+import requests
+from pathlib import Path
+
+"""
+TODO Need to take data from downloads and begin writing the relevant exif data next...
+if data is a zip, need to unzip and then also write that data into the image(s)
+
+Want to also find a way to unzip the file and then combine the video or jpg with its PNG
+
+At a good start right now, though, with it downloading wayyy faster than the HTML script
+
+Also, need to clean up, compartmentalize, and space out this code into functions/cleaner code
+
+"""
+
+# =========================================================================== #
+
+# Logic to parse user-provided html file for user-specific image info
+def parse_html():
+    target_string = "<div id='mem-info-bar'"
+    with open("memories_history.html", "r") as file:
+        for line in file:
+            if target_string in line:
+                html_text = line
+    return html_text
+
+# =========================================================================== #
 
 def parse_snapchat_memories(html_text):
     soup = BeautifulSoup(html_text, "html.parser")
@@ -45,16 +72,59 @@ def parse_snapchat_memories(html_text):
 
     return memories
 
+# =========================================================================== #
+
+def memory_download(memories):
+
+    total_files = len(memories)
+    download_count = 1
+
+    # Logic to begin downloading begins here
+    for line in memories:
+
+        url = line["url"]
+        name = line["date"]
+
+        name = name.replace(" ", "-")[:-4]
+        name = name.replace(":", "")
+
+        with requests.get(url, stream=True) as r:
+            r.raise_for_status()
+
+            content_type = r.headers.get("Content-Type", "").lower()
+
+            if "jpg" in content_type:
+                ext = ".jpg"
+            elif "png" in content_type:
+                ext = ".png"
+            elif "mp4" in content_type:
+                ext = ".mp4"
+            elif "zip" in content_type:
+                ext = ".zip"
+            else:
+                ext = ""  # fallback
+
+            out_dir = Path("./memories") # TODO give option for user-defined path in future
+            out_dir.mkdir(parents=True, exist_ok=True)
+
+            filepath = out_dir / f"{name}{ext}"
+
+            print(f"\rDownloading {download_count}/{total_files}...", end="", flush=True)
+
+            with open(filepath, 'wb') as f:
+                for chunk in r.iter_content(chunk_size=8192): # 8 KB chunks
+                    if chunk: # filter out keep-alive new chunks
+                        f.write(chunk)
+            download_count += 1
+
+    print() # final print to flush buffer and have newline
+
+# =========================================================================== #
+
 if __name__ == "__main__":
 
+    html_text = parse_html()
 
-    target_string = "<div id='mem-info-bar'"
+    memories = parse_snapchat_memories(html_text)
 
-    with open("memories_history.html", "r") as file:
-        for line in file:
-            if target_string in line:
-                html_text = line
-
-
-    print(html_text)
-#    print(parse_snapchat_memories(html_text))
+    memory_download(memories)
